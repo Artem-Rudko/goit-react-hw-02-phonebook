51c0cc6115a85f3b8b7b5d94770f759dd30bb361
Artem-Rudko <72283896+Artem-Rudko@users.noreply.github.com>
2021-07-14 00:07:32 +0300

    add new conctact Function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
+import { Component } from 'react';
+import ContactForm from './components/ContactForm';
+import { v4 as uuidv4 } from 'uuid';
 // import './App.css';
 
-function App() {
-    return (
-        <div className="App">
-            <h1>PHONEBOOK</h1>
-        </div>
-    );
+class App extends Component {
+    state = {
+        contacts: [
+            { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+            { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+            { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+            { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        ],
+        filter: '',
+    };
+
+    addNewContact = data => {
+        console.log(data);
+        const newContact = {
+            ...data,
+            id: uuidv4(),
+        };
+        console.log(newContact);
+        this.setState(prevState => ({
+            contacts: [...prevState.contacts, newContact],
+        }));
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Phonebook</h1>
+                <ContactForm onSubmit={this.addNewContact} />
+                <h2>Contacts</h2>
+            </div>
+        );
+    }
 }
 
 export default App;
